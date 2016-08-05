2e5af9f7bd90d33c939fc4fc1494e69a311c79f8
Eshu Goel <eshug@exzeo.com>
2016-08-06 03:51:50 +0530

    Updated config of webpack'

--- Abu/webpack.config.js
+++ Abu/webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
         alias: {
             Config: 'app/config/Config.jsx',
             Login: 'app/components/login/Login.jsx',
-            LoginForm: 'app/components/login/LoginForm.jsx'
+            LoginForm: 'app/components/login/LoginForm.jsx',
+            Dashboard: 'app/components/dashboard/Dashboard.jsx',
+            Header: 'app/components/dashboard/Header.jsx',
+            SearchService: 'app/components/dashboard/SearchService.jsx',
+            ServiceList: 'app/components/dashboard/ServiceList.jsx',
+            AddService: 'app/components/dashboard/AddService.jsx',
+            AddServiceForm: 'app/components/dashboard/AddServiceForm.jsx'
         },
         extensions: ['','.js','.jsx']
     },
