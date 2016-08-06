e224ee1564255dffbf02615d02eae013376c8d56
Eshu Goel <eshug@exzeo.com>
2016-08-06 05:38:53 +0530

    Implement some functionality on add service.

--- Abu/app/config/Config.jsx
+++ Abu/app/config/Config.jsx
@@ -1,6 +1,7 @@
 var config = {};
 
-config.baseUrl = 'http://172.16.1.101:8600';
+config.baseUrl = 'http://172.16.1.101:8081';
 config.loginUrl = '/info';
+config.info = '/info';
 
 module.exports = config;
