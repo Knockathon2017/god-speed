d23e83f7927307089887dda6480b4c9044741862
Eshu Goel <eshug@exzeo.com>
2016-08-06 11:11:27 +0530

    Implement add functionality.

--- Abu/app/components/dashboard/Dashboard.jsx
+++ Abu/app/components/dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var ServiceList = require('ServiceList');
 var Header = require('Header');
 
-
 var Dashboard = React.createClass({
     getInitialState: function () {
         return {
